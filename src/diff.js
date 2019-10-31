00d994483cb1a2e6e118ed1936cf89adf7365288
Akseli Palén <akseli.palen@gmail.com>
2019-10-31 21:27:55 +0200

    Setup async and sharp

--- src/diff.js
+++ src/diff.js
@@ -1,9 +1,23 @@
-// const sharp = require('sharp')
+const sharp = require('sharp')
+const path = require('path')
 const fs = require('fs')
+const asyn = require('async')
 
 module.exports = (dirpath) => {
   const files = fs.readdirSync(dirpath)
-  files.forEach(f => {
-    console.log(f)
+  const isImage = /\.(jpg|jpeg|png|tiff)$/i
+  const imageFiles = files.filter(f => {
+    return isImage.test(f)
+  })
+
+  asyn.eachSeries(imageFiles, (f, next) => {
+    const p = path.join(dirpath, f)
+    sharp(p)
+      .raw()
+      .toBuffer((err, data, info) => {
+        console.log(f, info.size)
+      })
+  }, (err) => {
+    console.log('finished')
   })
 }
