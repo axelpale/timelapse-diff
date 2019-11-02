2bb84e9a685a92e44ff8f7dd6f152b7ee84a4d49
Akseli Palén <akseli.palen@gmail.com>
2019-11-02 14:05:17 +0200

    Working diff output

---
+++ examples/sony-as50/index.js
@@ -0,0 +1,8 @@
+const diff = require('../../index')
+
+const p = '/Users/xeli/Desktop/werkenring/100MSDCF'
+
+diff.diff(p, {
+  minDiff: 150000000,
+  maxDiff: 1000000000
+})
--- src/diff.js
+++ src/diff.js
@@ -3,21 +3,58 @@ const path = require('path')
 const fs = require('fs')
 const asyn = require('async')
 
-module.exports = (dirpath) => {
+module.exports = (dirpath, opts) => {
+  // Default options
+  opts = Object.assign({
+    minDiff: 0,
+    maxDiff: Infinity
+  }, opts)
+
   const files = fs.readdirSync(dirpath)
   const isImage = /\.(jpg|jpeg|png|tiff)$/i
   const imageFiles = files.filter(f => {
     return isImage.test(f)
   })
 
+  let previousImage = null
+
   asyn.eachSeries(imageFiles, (f, next) => {
     const p = path.join(dirpath, f)
     sharp(p)
       .raw()
       .toBuffer((err, data, info) => {
-        console.log(f, info.size)
+        // data is an array of unsigned 8-bit integers.
+        // c bytes per pixel
+        // const c = info.channels
+        // info.width
+        // info.height
+
+        if (err) {
+          return next(err)
+        }
+
+        if (previousImage) {
+          let diffSum = 0
+          for (let i = 0; i < data.length; i += 1) {
+            diffSum += Math.abs(previousImage[i] - data[i])
+          }
+
+          if (diffSum < opts.minDiff || diffSum > opts.maxDiff) {
+            // Outlier, something we want to detect.
+            console.log(f, 'diff:', diffSum)
+          }
+        } else {
+          console.log(f, 'first')
+        }
+
+        previousImage = data
+
+        next()
       })
-  }, (err) => {
+  }, (finalerr) => {
+    if (finalerr) {
+      throw finalerr
+    }
     console.log('finished')
   })
 }
